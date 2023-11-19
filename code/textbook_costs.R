55c5c081c617e44244fdb201b4348bc1696245f3
MCWalstrum <marisa.c.walstrum@gmail.com>
2023-11-19 12:17:51 -0600

    added a line graph for inflation

--- code/textbook_costs.R
+++ code/textbook_costs.R
@@ -1,7 +1,7 @@
 library(blsR)
 library(tidyverse)
 # Note: Use bls_set_key() to set the BLS API key.
-
+bls_set_key("83b591c5fa1f4b308aee6c6bbaf7c0c3")
 # Download data from the BLS.
 cpi_data_raw <-
   get_n_series_table(
@@ -14,15 +14,23 @@ cpi_data_raw <-
   )
 
 # Set each index to be the percent change in price since January 2002.
+#Got help with math for this step...
 cpi_data_clean <- 
   cpi_data_raw |>
   mutate(
     inflation = 100 * (inflation / 177.7 - 1),
     tuition = 100 * (tuition / 360.7 - 1),
-    textbooks = 100 * (textbooks / 101.9 - 1)
+    textbooks = 100 * (textbooks / 101.9 - 1),
+    period = as.numeric(str_remove(period, "M")),
+    date= make_date(year, period)
   )
 
 # Make a line plot of the data.
+plot(
+  cpi_data_clean$date, 
+  cpi_data_clean$inflation, 
+  type ="l")
+
 
 
 
